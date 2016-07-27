c0f8d92fdea26dc98e2e10bf10f26f61c1073243
Chinyere Ukomadu <CUkomadu@Chinyeres-MacBook-Pro.local>
2016-07-26 21:28:34 -0500

    Created Backbone PryrRouter to handle routes to different app views

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,10 +3,61 @@ import ReactDOM from 'react-dom'
 import Backbone from 'backbone'
 import init from './init'
 import { User, PryrModel, PryrCollection } from './models/models'
+import MyPryrs from './views/MyPryrs'
+import PryrEditor from './views/pryrEditor'
+import LogIn from './views/login'
+import SignUp from './views/signup'
+import Home from './views/home'
 
 
 const app = function() {
-  //document.querySelector('.container').innerHTML = "<h1>Woah!</h1>"
+ 
+  const PryrRouter = Backbone.Router.extend({
+  	routes: {
+  		"/pryrs/read": "_showMyPryrs",
+  		"/pryrs/create": "_showPryrEditor",
+  		"login": "_showLogin",
+  		"signup": "_showSignUp",
+  		"home": "_showHome",
+  		"*catchall": "_redirect"
+  	},
+
+  	_showAllPryrs: function(){
+  		console.log('this is MyPryrs')
+  		ReactDOM.render(<MyPryrs />, document.querySelector('.container'))
+  	}
+
+  	_showPryrEditor: function(){
+  		console.log('this is PryrEditor')
+  		ReactDOM.render(<PryrEditor />, document.querySelector('.container'))
+  	},
+
+  	_showLogin: function(){
+  		console.log('this is Login')
+  		ReactDOM.render(<LogIn />, document.querySelector('.container'))
+  	},
+
+  	_showSignUp: function(){
+  		console.log('this is Sign Up')
+  		ReactDOM.render(<SignUp />, document.querySelector('.container'))
+  	},
+
+  	_showHome: function(){
+  		console.log('this is Home')
+  		ReactDOM.render(<Home />, document.querySelector('.container'))
+  	},
+
+  	_redirect: function(){
+  		location.hash = "home"
+  	},
+
+  	initialize: function(){
+  		Backbone.history.start()
+  	}
+
+  })
+
+  new PryrRouter()
 }
 
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
---
+++ src/scripts/views/header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ACTIONS from '../actions'
+
+const Header = React.createClass({
+	render: function(){
+		return (
+				<div>
+					<h1 id="Header">PryrWorks</h1>
+					<NavBar />
+				</div>
+			)
+	}
+})
+
+const NavBar = React.createClass({
+	render: function(){
+		return (
+				<div className="NavBar aLinks">
+					<a href="#home">Home</a>
+					<a href="#login">Log In</a>
+					<a href="#signup">Sign Up</a>
+					<a href="#messages/read">Your Prayers</a>
+					<a href="#messages/write">Add Prayer</a>
+					<a href="#" onClick={ACTIONS.logUserOut} >Log Out</a>
+				</div>
+			)
+	}
+})
+
+export default Header
