85e1763e10271706f511046b905c90d778be08c5
Chinyere Ukomadu <CUkomadu@Chinyeres-MacBook-Pro.local>
2016-07-26 21:50:41 -0500

    Created Home View aka Landing Page

---
+++ src/scripts/views/home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './header'
+
+const Home = React.createClass({
+	render: function(){
+		return(
+				<div>
+					<Header />
+					<div>
+						<h2>DOES PRAYER WORK? TRACK AND SEE!</h2>
+						<p>The World's Leading Prayer Organizer for measuring the effectiveness of prayer</p>
+					</div>
+					<div>
+					</div>
+				</div>
+			)
+	}
+})
+
+export default Home
